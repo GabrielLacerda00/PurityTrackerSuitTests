c703682c7bbba742fc4024f3ed86f1f753cb96f1
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2023-09-26 14:47:02 -0300

    code before refactoring

---
+++ OneRenameAndTwoMethodCalls3.0/Calculator/src/main/java/org/example/Calculator.java
@@ -0,0 +1,25 @@
+package org.example;
+
+
+public class Calculator {
+	
+	public static int add(int a, int b) {
+		return a + b;
+	}
+	
+	public int subtract(int a, int b) {
+		return a-b;
+	}
+	
+	public int multiply(int a, int b) {
+		return (add(a,b)) * b;
+	}
+	
+	public float division(int a, int b) {
+		return  add(a,b) / b;
+	}
+
+	public static void main (String args[]) {
+		add(5,7);
+	}
+}
