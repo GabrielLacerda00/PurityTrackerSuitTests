1a248f1bc0be6e49635908c76c0f5d71a55a42e3
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2023-09-27 00:51:30 -0300

    code before refactoring

---
+++ OneRenameForMissingObjCallersInOtherClasses/Calculator/src/main/java/org/example/Calculator.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Calculator {
+
+    public  int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public float division(int a, int b) {
+        return a / b;
+    }
+
+
+    public static void main (String args[]) {
+        Calculator cal = new Calculator();
+        cal.add(5,6);
+        cal.add(4,7);
+    }
+}
---
+++ OneRenameForMissingObjCallersInOtherClasses/Calculator/src/main/java/org/example/MyObj.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class MyObj  extends Calculator{
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+
+    public static void main (String args[]) {
+        Calculator cal = new Calculator();
+        cal.add(4,5);
+        cal.add(6,8);
+    }
+}
